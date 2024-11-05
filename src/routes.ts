c972ffeb0981ae6cbf2d0714568eb6ae37a56c20
elle4ry <138738430+elle4ry@users.noreply.github.com>
2024-11-05 19:46:50 -0300

    updateUm

--- src/routes.ts
+++ src/routes.ts
@@ -1,32 +1,31 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 import { recipesData } from './data';
 
 const routes = Router();
 
 // Exemplo de endpoint GET
-routes.get('/recipes/:name', (req: Request, res: Response) => {
+routes.get('/recipes/:name', (req, res) => {
     const { name } = req.params;
     const recipe = recipesData.find(recipe => recipe.name.toLowerCase() === name.toLowerCase());
 
     if (recipe) {
-        return res.json(recipe);
+        res.json(recipe);
     } else {
-        return res.status(404).json({ message: 'Receita não encontrada' });
+        res.status(404).json({ message: 'Receita não encontrada' });
     }
 });
 
 // Exemplo de endpoint POST
-routes.post('/recipes', (req: Request, res: Response) => {
+routes.post('/recipes', (req, res) => {
     const { name, ingredients, boosts, npcPreferences, rarity } = req.body;
 
     if (!name || !ingredients || !boosts || !npcPreferences || !rarity) {
-        return res.status(400).json({ message: 'Por favor, forneça todos os campos necessários' });
+        res.status(400).json({ message: 'Por favor, forneça todos os campos necessários' });
+    } else {
+        const newRecipe = { name, ingredients, boosts, npcPreferences, rarity };
+        recipesData.push(newRecipe);
+        res.status(201).json({ message: 'Receita adicionada com sucesso', recipe: newRecipe });
     }
-
-    const newRecipe = { name, ingredients, boosts, npcPreferences, rarity };
-    recipesData.push(newRecipe);
-
-    return res.status(201).json({ message: 'Receita adicionada com sucesso', recipe: newRecipe });
 });
 
 export { routes };
